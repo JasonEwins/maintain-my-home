155956f40cd598f9eba670bc1ea66396bf46add4
Robert Lee-Cann <leeky@leeky.org.uk>
2017-09-26 15:03:00 +0100

    (feature) Implement the first question
    Show users the first question and two possible answers. Depending
    on the chosen answer, the user is either redirected to the emergency
    contacts page, or shown the next question (currently, faked in
    view of `Questions::StartController#submit`).
    
    In the case where the form is submitted without choosing an option, the
    users are shown a warning message and the form is redisplayed.

--- app/controllers/questions/start_controller.rb
+++ app/controllers/questions/start_controller.rb
@@ -3,5 +3,26 @@ module Questions
     def index
       @form = StartForm.new
     end
+
+    def submit
+      @form = StartForm.new(start_form_params)
+
+      unless @form.valid?
+        return redirect_to(
+          questions_start_path,
+          alert: @form.errors.full_messages
+        )
+      end
+
+      return redirect_to emergency_contact_path if @form.priority_repair?
+
+      render :submit
+    end
+
+    private
+
+    def start_form_params
+      params.require(:start_form).permit!
+    end
   end
 end
